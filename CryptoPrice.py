d72c7b41198b84f4c971b27cf742cb86672bd536
JasonBTW23 <164063136+JasonBTW23@users.noreply.github.com>
2024-10-09 13:37:40 -0500

    Update CryptoPrice.py

--- CryptoPrice.py
+++ CryptoPrice.py
@@ -1,5 +1,5 @@
 import requests
-# import pandas might use instead of params
+import pandas
 import sqlalchemy
 apikey = '0abd643d-332d-4d9d-8ee0-87cd0c97289d'
 
@@ -25,6 +25,7 @@ for x in crypto:
   print(x['symbol'],x['quote']['USD']['price'])
 
 
-engine = sqlalchemy.create_engine()  #connection between python code and sql server
-crypto.to_sql()#two sql method to push the data to the table
+engine = sqlalchemy.create_engine("mysql+pymysql://scott:tiger@localhost/foo")  #connection between python code and sql server
+crypto.to_sql(name='table_name', con=conn, if_exists='replace', index=False) #two sql method to push the data to the table
 
+#engine.close()
