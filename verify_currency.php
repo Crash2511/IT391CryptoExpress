cb8ae6846f6310808ed98701d015e59c050ca541
Crash2511 <129631232+Crash2511@users.noreply.github.com>
2024-12-10 22:36:59 -0600

    Update verify_currency.php

--- verify_currency.php
+++ verify_currency.php
@@ -4,8 +4,8 @@ session_start();
 // Database connection settings
 $servername = "localhost";
 $username = "user";
-$password = "Battle2511!";
-$dbname = "crypto_express";
+$password = ""; // your password here
+$dbname = "crypto_express"; // Edit db if necessary here
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
